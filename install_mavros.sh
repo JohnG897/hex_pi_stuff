9fe17ce0ff7c00b75ad5068617287425c1dc2f67
JohnG897 <jwgifford1@sheffield.ac.uk>
2018-02-28 19:34:25 +0000

    adding mavros install file

--- install_mavros.sh
+++ install_mavros.sh
@@ -1,27 +1,16 @@
 #!/bin/bash 
+log_file=$PWD"/install_mavros.txt"
 cd ~/
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python-pip python-setuptools python-yaml python-distribute python-docutil python-dateutil python-six
-sudo pip install rosdep rosinstall_generator wstool rosinstall
-sudo rosdep init
-rosdep update
-mkdir ~/ros_catkin_ws
-cd ~/ros_catkin_ws
-
-rosinstall_generator ros_comm --rosdistro indigo --deps --wet-only --exclude roslisp --tar > indigo-ros_comm-wet.rosinstall
-wstool init src indigo-ros_comm-wet.rosinstall
-
-cd ~/ros_catkin_ws/external_src
-sudo apt-get install libboost-filesystem-dev libxml2-dev
-wget http://downloads.sourceforge.net/project/collada-dom/Collada%20DOM/Collada%20DOM%202.4/collada-dom-2.4.0.tgz
-tar -xzf collada-dom-2.4.0.tgz
-cd collada-dom-2.4.0
-sudo checkinstall make install
-
-cd ~/ros_catkin_ws
-rosdep install --from-paths src --ignore-src --rosdistro indigo -y -r --os=debian:jessie
-source /opt/ros/indigo/setup.bash
-
-
-sudo apt-get install ros-indigo-mavros ros-indigo-mavros-extras
+rm $log_file
+sudo apt-get update | tee -a $log_file
+sudo apt-get upgrade | tee -a $log_file
+mkdir -p ~/catkin_ws/src | tee -a $log_file
+cd ~/catkin_ws | tee -a $log_file
+catkin init | tee -a $log_file
+sudo apt-get install python-wstool python-rosinstall-generator python-catkin-tools | tee -a $log_file
+wstool init ~/catkin_ws/src | tee -a $log_file
+rosinstall_generator --rosdistro indigo --upstream mavros --deps | tee /tmp/mavros.rosinstall | tee -a $log_file
+rosinstall_generator --rosdistro indigo mavlink --deps | tee -a /tmp/mavros.rosinstall | tee -a $log_file
+wstool merge -t src /tmp/mavros.rosinstall | tee -a $log_file
+wstool update -t src | tee -a $log_file
+rosdep install --from-paths src --ignore-src --rosdistro `echo $ROS_DISTRO` -y | tee -a $log_file
