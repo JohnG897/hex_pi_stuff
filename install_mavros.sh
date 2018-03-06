223dc45798ec39b712304685efa28a2880b65d59
Malvin Todorov <malvintodorov@gmail.com>
2018-03-06 16:06:14 +0000

    Refactor and remove reduntant logging

--- install_mavros.sh
+++ install_mavros.sh
@@ -2,15 +2,20 @@
 log_file=$PWD"/install_mavros.txt"
 cd ~/
 rm $log_file
-sudo apt-get update | tee -a $log_file
-sudo apt-get upgrade | tee -a $log_file
-mkdir -p ~/catkin_ws/src | tee -a $log_file
-cd ~/catkin_ws | tee -a $log_file
-catkin init | tee -a $log_file
-sudo apt-get install python-wstool python-rosinstall-generator python-catkin-tools | tee -a $log_file
-wstool init ~/catkin_ws/src | tee -a $log_file
-rosinstall_generator --rosdistro indigo --upstream mavros --deps | tee /tmp/mavros.rosinstall | tee -a $log_file
-rosinstall_generator --rosdistro indigo mavlink --deps | tee -a /tmp/mavros.rosinstall | tee -a $log_file
-wstool merge -t src /tmp/mavros.rosinstall | tee -a $log_file
-wstool update -t src | tee -a $log_file
-rosdep install --from-paths src --ignore-src --rosdistro `echo $ROS_DISTRO` -y | tee -a $log_file
+
+function install_mavros(){
+    sudo apt-get update
+    sudo apt-get upgrade
+    mkdir -p ~/catkin_ws/src
+    cd ~/catkin_ws
+    catkin init
+    sudo apt-get install python-wstool python-rosinstall-generator python-catkin-tools
+    wstool init ~/catkin_ws/src
+    rosinstall_generator --rosdistro indigo --upstream mavros --deps | tee /tmp/mavros.rosinstall
+    rosinstall_generator --rosdistro indigo mavlink --deps | tee -a /tmp/mavros.rosinstall
+    wstool merge -t src /tmp/mavros.rosinstall
+    wstool update -t src
+    rosdep install --from-paths src --ignore-src --rosdistro `echo $ROS_DISTRO` -y
+}
+
+install_mavros | tee -a $log_file
